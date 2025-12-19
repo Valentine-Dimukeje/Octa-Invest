d51ffc2dadedf99b8d0f018a883f9020608ee7c1
Valentine-Dimukeje <valentinedimukeje@gmail.com>
2025-12-19 08:51:03 +0100

    Remove unsupported webpack override (top-level await)

--- src/components/config.overrides.js
+++
@@ -1,9 +0,0 @@
-module.exports = function override(config, env) {
-  // Enable top-level await
-  config.experiments = {
-    ...config.experiments,
-    topLevelAwait: true,
-  };
-  
-  return config;
-};
